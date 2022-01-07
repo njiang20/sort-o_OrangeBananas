f657ad19342df2fd3ceaea90251e9aaa10b1e79f
Nina Jiang <njiang20@stuy.edu>
2022-01-07 13:55:30 -0500

    lab04

--- Driver.java
+++ Driver.java
@@ -5,15 +5,15 @@ public class Driver {
 
 	public static void main(String [] args) {
 			ArrayList AL = Sorts.populate(10, 1, 100);
-      System.out.println( "ArrayList AL before sorting:\n" + AL );
+      System.out.println( "\nArrayList AL before sorting:\n" + AL );
 
-			System.out.println( "ArrayList AL after bubble sorting:\n");
+			System.out.println( "\nArrayList AL after bubble sorting:\n");
       System.out.println(Sorts.bubbleSort(AL));
 
-			System.out.println( "ArrayList AL after insertion sorting:\n");
+			System.out.println( "\nArrayList AL after insertion sorting:\n");
       System.out.println(Sorts.insertionSort(AL));
 
-			System.out.println( "ArrayList AL after selection sorting:\n");
+			System.out.println( "\nArrayList AL after selection sorting:\n");
       System.out.println(Sorts.selectionSort(AL));
 	}
 }
